2315be841b11415f924eff7def95f37d47d39f91
Jenise Ruehle <jela07@gmail.com>
2020-08-12 19:36:23 +0000

    Automatically backed up by Learn

--- db/migrate/002_create_costume_stores.rb
+++ db/migrate/002_create_costume_stores.rb
@@ -1,12 +1,12 @@
 class CreateCostumeStores < ActiveRecord::Migration[5.2]
   
   def change 
-    create_table :costumes do |t|
-      t.string :name
-      t.integer :price
-      t.string :size 
-      t.string :image_url
-      t.timestamps
-    end 
+    # create_table :costumes do |t|
+    #   t.string :name
+    #   t.integer :price
+    #   t.string :size 
+    #   t.string :image_url
+    #   t.timestamps
+    # end 
   end 
 end 
